4979357805b9eb8a57883a326485a5781f6569d9
Nicolas Limage <github@xephon.org>
2016-04-15 11:51:48 +0200

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name = "cliprompt",
-    version = "0.1",
+    version = "0.2",
     packages = ['cliprompt'],
     author = "Nicolas Limage",
     author_email = 'github@xephon.org',
